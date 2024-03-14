acaa0661e9abe0572cb39e7e69ca75fcd75b04c0
João R. Augusto <contato@joaoaugustodev.com.br>
2024-03-14 17:39:04 -0300

    feat(domain/entity): Adicionado a estrutura das entidades do projeto

--- src/main/java/com/wayon/domain/transaction/Transaction.java
+++ src/main/java/com/wayon/domain/transaction/Transaction.java
@@ -1,4 +1,23 @@
 package com.wayon.domain.transaction;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fromAccount;
+    private String toAccount;
+    private double value;
+    private LocalDate scheduleDateTransaction;
 }
--- src/main/java/com/wayon/domain/user/User.java
+++ src/main/java/com/wayon/domain/user/User.java
@@ -1,4 +1,22 @@
 package com.wayon.domain.user;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "users")
+@Table(name = "users")
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long account;
+    private String name;
+    private BigDecimal balance;
 }
