95ed477eba00e0c7c0d36f4dcb6a47775297d1c0
João R. Augusto <contato@joaoaugustodev.com.br>
2024-03-15 18:05:09 -0300

    fix: ajustado tipo de conta e adição do construtor para salvar usuário

--- src/main/java/com/wayon/domain/user/User.java
+++ src/main/java/com/wayon/domain/user/User.java
@@ -1,11 +1,12 @@
 package com.wayon.domain.user;
 
+import com.wayon.dtos.UserDto;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
 
-@Entity(name = "users")
+@Entity
 @Table(name = "users")
 @EqualsAndHashCode(of = "id")
 @Getter
@@ -15,8 +16,15 @@ import java.math.BigDecimal;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private Long account;
-    private String name;
-    private BigDecimal balance;
+    public Long id;
+    @Column(unique = true)
+    public String account;
+    public String name;
+    public double balance;
+
+    public User(UserDto userDto) {
+        this.name = userDto.name;
+        this.account = userDto.account;
+        this.balance = userDto.balance;
+    }
 }
