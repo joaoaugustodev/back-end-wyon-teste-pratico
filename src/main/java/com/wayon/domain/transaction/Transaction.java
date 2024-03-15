195b9dcde95ddfef0c6bb3a9e92d7a6d038e8897
João R. Augusto <contato@joaoaugustodev.com.br>
2024-03-15 18:04:02 -0300

    fix: corrigido nome do campo value para valueTransaction e adição do UserDto para salvar usuário

--- src/main/java/com/wayon/domain/transaction/Transaction.java
+++ src/main/java/com/wayon/domain/transaction/Transaction.java
@@ -1,11 +1,12 @@
 package com.wayon.domain.transaction;
 
+import com.wayon.dtos.TransactionDto;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
 
-@Entity(name = "transactions")
+@Entity
 @Table(name = "transactions")
 @EqualsAndHashCode(of = "id")
 @Getter
@@ -15,9 +16,20 @@ import java.time.LocalDate;
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String fromAccount;
-    private String toAccount;
-    private double value;
-    private LocalDate scheduleDateTransaction;
+    public Long id;
+    public String fromAccount;
+    public String toAccount;
+    public double valueTransaction;
+    public double moneyFee;
+    public double percentFee;
+    public LocalDate scheduleDateTransaction;
+
+    public Transaction(TransactionDto transactionDto, double moneyFee, double percentFee) {
+        this.fromAccount = transactionDto.fromAccount;
+        this.toAccount = transactionDto.toAccount;
+        this.valueTransaction = transactionDto.valueTransaction;
+        this.moneyFee = moneyFee;
+        this.percentFee = percentFee;
+        this.scheduleDateTransaction = transactionDto.transactionDate;
+    }
 }
