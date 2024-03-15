ce1b9898e26efb5fe9a3a75fefb9e71f6c1bfd68
João R. Augusto <contato@joaoaugustodev.com.br>
2024-03-15 18:06:23 -0300

    feat(repositories): atualização do repositories user e transaction

--- src/main/java/com/wayon/repositories/TransactionRepository.java
+++ src/main/java/com/wayon/repositories/TransactionRepository.java
@@ -2,6 +2,11 @@ package com.wayon.repositories;
 
 import com.wayon.domain.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    @Query(value = "SELECT * FROM transactions WHERE from_account = ?1", nativeQuery = true)
+    List<Transaction> getAllTransactionByAccount(String fromAccount);
 }
--- src/main/java/com/wayon/repositories/UserRepository.java
+++ src/main/java/com/wayon/repositories/UserRepository.java
@@ -2,6 +2,11 @@ package com.wayon.repositories;
 
 import com.wayon.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query(value = "SELECT * FROM users WHERE account = ?1", nativeQuery = true)
+    User getAllUsersByAccount(String account);
 }
