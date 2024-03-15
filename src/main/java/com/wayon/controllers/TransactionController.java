60fe28636beb8c3f184ff6eca6ba8fdb423e6b64
João R. Augusto <contato@joaoaugustodev.com.br>
2024-03-15 18:06:00 -0300

    feat(controllers): atualização do controllers user e transaction

--- src/main/java/com/wayon/controllers/TransactionController.java
+++ src/main/java/com/wayon/controllers/TransactionController.java
@@ -1,9 +1,60 @@
 package com.wayon.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.wayon.domain.fee.Fee;
+import com.wayon.domain.transaction.Transaction;
+import com.wayon.domain.user.User;
+import com.wayon.dtos.TransactionDto;
+import com.wayon.services.FeeService;
+import com.wayon.services.TransactionService;
+import com.wayon.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/transaction")
 public class TransactionController {
+    @Autowired
+    public TransactionService transactionService;
+    @Autowired
+    public FeeService feeService;
+    @Autowired
+    public UserService userService;
+
+    @GetMapping("/{account}")
+    public ResponseEntity<List<Transaction>> getAllTransactionByAccount(@PathVariable String account) {
+        List<Transaction> transactions = this.transactionService.allTransactionByAccount(account);
+        return new ResponseEntity<>(transactions, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Transaction> addTransaction(@RequestBody TransactionDto transactionDto) throws Exception {
+        Integer diff = transactionService.getDiffDay(LocalDate.now(), transactionDto.transactionDate);
+        Fee fee = feeService.getFeeFromDiffDay(diff);
+        User sender = userService.getUserByAccount(transactionDto.fromAccount);
+        User receiver = userService.getUserByAccount(transactionDto.toAccount);
+        boolean hasBalance = sender.balance >= transactionDto.valueTransaction;
+        boolean isReceiverSender = transactionDto.fromAccount.equals(transactionDto.toAccount);
+
+        if (fee == null || isReceiverSender) {
+            throw new Exception("Transferência não concluída.");
+        }
+
+        if (!hasBalance || !(transactionDto.valueTransaction > fee.moneyFee)) {
+            throw new Exception("Sem saldo suficiente ou valor de tranferência menor que o valor da taxa.");
+        }
+
+        double receiverBalance = transactionDto.valueTransaction * (fee.percentFee / 100) + fee.moneyFee;
+        sender.balance = sender.balance - transactionDto.valueTransaction;
+        receiver.balance = receiver.balance + (transactionDto.valueTransaction - receiverBalance);
+        userService.updateUser(sender);
+        userService.updateUser(receiver);
+        Transaction response = transactionService.makeTransaction(transactionDto, fee);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/wayon/controllers/UserController.java
+++ src/main/java/com/wayon/controllers/UserController.java
@@ -1,9 +1,48 @@
 package com.wayon.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.wayon.domain.user.User;
+import com.wayon.dtos.UserDto;
+import com.wayon.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    public UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody UserDto user) {
+        User newUser = this.userService.createUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = this.userService.allUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) throws Exception {
+        User user = this.userService.findUserById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PutMapping("/{account}")
+    public ResponseEntity<User> updateUser(@RequestBody UserDto user, @PathVariable String account) {
+        User updatedUser = userService.getUserByAccount(account);
+
+        updatedUser.account = user.account;
+        updatedUser.name = user.name;
+        updatedUser.balance = user.balance;
+
+        userService.updateUser(updatedUser);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
 }
