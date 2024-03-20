8b82fb588f386942ee558d08d28b681ed7b77018
João R. Augusto <contato@joaoaugustodev.com.br>
2024-03-20 12:50:12 -0300

    fix: add getter e setters

--- src/main/java/com/wayon/domain/user/User.java
+++ src/main/java/com/wayon/domain/user/User.java
@@ -6,27 +6,54 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
-import java.math.BigDecimal;
-
 @Entity
 @Table(name = "users")
 @EqualsAndHashCode(of = "id")
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long id;
+    private Long id;
     @Column(unique = true)
     @Pattern(regexp = "^[0-9]{10}$")
     @NotBlank
-    public String account;
+    private String account;
     @NotBlank
-    public String name;
-    @NotBlank
-    public double balance;
+    private String name;
+    private double balance;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 
     public User(UserDto userDto) {
         this.name = userDto.name;
