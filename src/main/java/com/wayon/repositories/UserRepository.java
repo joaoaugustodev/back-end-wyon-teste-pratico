a1414a8a64c770d30345980a0019d59c92d1d33f
João R. Augusto <contato@joaoaugustodev.com.br>
2024-03-20 12:48:17 -0300

    fix: remoção do import sem utilizar

--- src/main/java/com/wayon/repositories/UserRepository.java
+++ src/main/java/com/wayon/repositories/UserRepository.java
@@ -4,8 +4,6 @@ import com.wayon.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT * FROM users WHERE account = ?1", nativeQuery = true)
     User getAllUsersByAccount(String account);
